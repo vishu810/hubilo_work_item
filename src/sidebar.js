59ee9d90e7b450cee6330a2a4411e2710a79d5b5
vishvajeetsingh-Tx <vishvajeet.singh@testingxperts.com>
2024-08-29 01:24:39 +0530

    removed unwanted things

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Drawer, IconButton, Checkbox, Button, List, ListItem, ListItemText,  Avatar, InputBase } from '@mui/material';
+import { Drawer, IconButton, Checkbox, Button, List, ListItem, ListItemText, Avatar, InputBase } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined'; 
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import { speakers } from './components/speaker';
-
 
 export default function Sidebar({ open, onClose }) {
     const [selectedSpeakers, setSelectedSpeakers] = React.useState([]);
@@ -18,60 +17,59 @@ export default function Sidebar({ open, onClose }) {
     };
 
     return (
-      
-        <Drawer  anchor="right" open={open} onClose={onClose}>
+
+        <Drawer anchor="right" open={open} onClose={onClose}>
             <div className="sidebar-header">
                 <h3>Add Speaker</h3>
                 <IconButton onClick={onClose}>
                     <CloseIcon />
                 </IconButton>
             </div>
-         <div className='search-bar-parent'>
-            <div  >
-                
-                <InputBase
-                    className="search-bar w-500"
-                    placeholder="   Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                    startAdornment={<SearchIcon style={{color:'#E4875D'}} />}
-                />
-            </div>
+            <div className='search-bar-parent'>
+                <div  >
+
+                    <InputBase
+                        className="search-bar w-500"
+                        placeholder="   Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                        startAdornment={<SearchIcon style={{ color: '#E4875D' }} />}
+                    />
+                </div>
             </div>
             <div className='parentCheckbox'>
-            <List className="sidebar-content"   >
-                {speakers.map(speaker => (
-                    <ListItem
-                        id='checkboxstyle'
-                        key={speaker.id}
-                        className={selectedSpeakers.includes(speaker.id) ? 'selected' : ''}
-                        // onClick={() => handleSelectSpeaker(speaker.id)}
-                    >
-                        <div>
-                        <div className='list-inside-section'>
-                            <Avatar></Avatar> 
+                <List className="sidebar-content"   >
+                    {speakers.map(speaker => (
+                        <ListItem
+                            id='checkboxstyle'
+                            key={speaker.id}
+                            className={selectedSpeakers.includes(speaker.id) ? 'selected' : ''}
+                        >
                             <div>
-                                <ListItemText
-                                    primary={speaker.name}
-                                    secondary={`${speaker.title} | ${speaker.company}`}
-                                />
+                                <div className='list-inside-section'>
+                                    <Avatar></Avatar>
+                                    <div>
+                                        <ListItemText
+                                            primary={speaker.name}
+                                            secondary={`${speaker.title} | ${speaker.company}`}
+                                        />
+                                    </div>
+
+
+                                    <Checkbox className='section-checkbox'
+                                        checked={selectedSpeakers.includes(speaker.id)}
+                                        onChange={() => handleSelectSpeaker(speaker.id)}
+                                    />
+                                </div>
+                                <div className='edit-button-parent'>
+                                    <IconButton className='edit-btn' edge="end" >
+                                        <EditOutlinedIcon />
+                                        <p>Edit Speaker</p>
+                                    </IconButton>
+                                </div>
                             </div>
-                            
-                            
-                                <Checkbox className='section-checkbox'
-                                    checked={selectedSpeakers.includes(speaker.id)}
-                                    onChange={() => handleSelectSpeaker(speaker.id)}
-                                />
-                        </div>
-                        <div className='edit-button-parent'>
-                        <IconButton  className='edit-btn' edge="end" >
-                            <EditOutlinedIcon  /> 
-                           <p>Edit Speaker</p> 
-                        </IconButton>
-                        </div>
-                        </div>
-                    </ListItem>
-                ))}
-            </List>
+                        </ListItem>
+                    ))}
+                </List>
             </div>
             <div className="sidebar-footer">
                 <div className='add-cancel-btn'>
@@ -95,8 +93,8 @@ export default function Sidebar({ open, onClose }) {
                     Create Speaker
                 </Button>
             </div>
-           
+
         </Drawer>
-    
+
     );
 }
